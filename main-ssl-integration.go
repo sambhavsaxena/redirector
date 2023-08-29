4ab63ff5895be44b8406a1765f5c64e1e3c22e9e
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2023-08-29 22:42:14 +0530

    add: `main-ssl-integration.go`

---
+++ main-ssl-integration.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "crypto/tls"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+)
+
+type Proxy struct {
+    target *url.URL
+    proxy  *httputil.ReverseProxy
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    log.Println(r.URL)
+    r.Host = p.target.Host
+    p.proxy.ServeHTTP(w, r)
+}
+
+func main() {
+    target, err := url.Parse("https://example.com")
+    if err != nil {
+        panic(err)
+    }
+    proxy := httputil.NewSingleHostReverseProxy(target)
+    proxy.Transport = &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+    p := &Proxy{target: target, proxy: proxy}
+    err = http.ListenAndServe(":3000", p)
+    if err != nil {
+        panic(err)
+    }
+}
